e82917906ddb0782a0e7425e9e2d061482473cc7
axelpicou <115532798+axelpicou@users.noreply.github.com>
2024-02-27 12:52:16 +0100

    player movement

---
+++ Assets/Player1/MovePlayer1.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MovePlayer1 : MonoBehaviour
+{
+    private float speed = 200f;
+    private float rotationspeed = 100f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("HorizontalPlayer1");
+        float vAxis = Input.GetAxis("VerticalPlayer1");
+        _rigidbody.velocity = (transform.forward * vAxis) * speed * Time.deltaTime;
+        transform.Rotate((transform.up * hAxis) * rotationspeed * Time.deltaTime);
+    }
+}
---
+++ Assets/player2/MovePlayer2.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MovePlayer2 : MonoBehaviour
+{
+    private float speed = 200f;
+    private float rotationspeed = 100f;
+
+    private Rigidbody _rigidbody;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float hAxis = Input.GetAxis("HorizontalPlayer2");
+        float vAxis = Input.GetAxis("VerticalPlayer2");
+        _rigidbody.velocity = (transform.forward * vAxis) * speed * Time.deltaTime;
+        transform.Rotate((transform.up * hAxis) * rotationspeed * Time.deltaTime);
+    }
+}
