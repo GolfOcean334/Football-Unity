61686aa4c6849fe52a0609983f3b71e7d701f6e3
GolfOcean334 <130740013+GolfOcean334@users.noreply.github.com>
2024-02-29 16:55:45 +0100

    changement collision

--- Assets/Script/CollisionBall.cs
+++ Assets/Script/CollisionBall.cs
@@ -1,30 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class CollisionBall : MonoBehaviour
+public class TriggerBall : MonoBehaviour
 {
     private PositionBall positionBall;
     private MovePlayer1 positionP1;
     private MovePlayer2 positionP2;
+
     void Start()
     {
         positionBall = FindObjectOfType<PositionBall>();
         positionP1 = FindObjectOfType<MovePlayer1>();
         positionP2 = FindObjectOfType<MovePlayer2>();
     }
-    private void OnCollisionEnter(Collision other)
+
+    private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Bluescored")
+        if (other.gameObject.CompareTag("Bluescored"))
         {
             scoreBlueteam.scorecount += 1;
             positionBall.PositionBaseBall(positionBall.positionBase);
             positionP1.PositionBaseP1(positionP1.positionBaseP1);
             positionP2.PositionBaseP2(positionP2.positionBaseP2);
-
         }
 
-        if (other.gameObject.tag == "Redscored")
+        if (other.gameObject.CompareTag("Redscored"))
         {
             scoreRedteam.scorecount += 1;
             positionBall.PositionBaseBall(positionBall.positionBase);
